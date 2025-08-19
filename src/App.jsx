cc6953ab32b73058358003f69f71cfdf5a4829f2
Yongting <yongtingshi657@gmail.com>
2025-08-19 17:10:44 -0400

    files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import TodoList from './TodoList';
-import TodoForm from './TodoForm';
+import TodoList from './features/TodoList/TodoList';
+import TodoForm from './features/TodoForm';
 import { useState } from 'react';
 
 function App() {
